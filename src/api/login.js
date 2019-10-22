43de6f48ad5bd7d9d04f509ad9a65466ff176967
pcx <13480375727@163.com>
2019-10-22 17:02:15 +0800

    提交

---
+++ src/api/login.js
@@ -0,0 +1,27 @@
+import request from '@/utils/request'
+
+export function login(username, password) {
+  console.dir(username)
+  return request({
+    url: '/admin/login',
+    method: 'post',
+    data: {
+      username,
+      password
+    }
+  })
+}
+
+export function getInfo() {
+  return request({
+    url: '/admin/info',
+    method: 'get',
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/admin/logout',
+    method: 'post'
+  })
+}
